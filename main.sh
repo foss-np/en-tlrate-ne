6964c34b670ab2d3becb0d723f6f79e3ff462e51
Rhoit <rho.rhoit@gmail.com>
2012-06-03 13:59:33 +0545

    added expression modifier

--- main.sh
+++ main.sh
@@ -3,11 +3,14 @@
 # convert number
 	sed -f expnum string > strnum
 
-# first iteration
+# literal scan
 	sed -f expfull strnum > string1
 
+# double modifer
+	sed -f expmod string1 > string2
+
 # full the half thing
-	sed -f exphalf string1
+	sed -f exphalf string2
 
 rm string1 strnum
 
